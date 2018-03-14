ed72bc1936565f9bf74a4f9761113eb18ae31dc1
Michael A. Floyd <mafloyd23@gmail.com>
2018-03-14 18:37:22 -0500

    Base.html is now functioning.

--- phunt/settings.py
+++ phunt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'phunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['phunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'phunt/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
